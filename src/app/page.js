a92bfbc56e6c9ba0dc4dcee6d6198b537a8ae2d5
harzeezco <wakilabdulazeez@gmail.com>
2025-01-23 22:58:46 +0100

    milestone completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,56 +1,32 @@
+'use client';
+
+import { useScroll, useTransform, motion } from 'framer-motion';
 import Image from 'next/image';
+import { useRef } from 'react';
 
 export default function Home() {
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ['start start', 'end start'],
+	});
+
+	const scale = useTransform(scrollYProgress, [0, 1], [1, 0]);
+
 	return (
-		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-			hello world
-			<footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
-				<a
-					className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-					href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<Image
-						aria-hidden
-						src='/file.svg'
-						alt='File icon'
-						width={16}
-						height={16}
-					/>
-					Learn
-				</a>
-				<a
-					className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-					href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<Image
-						aria-hidden
-						src='/window.svg'
-						alt='Window icon'
-						width={16}
-						height={16}
-					/>
-					Examples
-				</a>
-				<a
-					className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-					href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<Image
-						aria-hidden
-						src='/globe.svg'
-						alt='Globe icon'
-						width={16}
-						height={16}
-					/>
-					Go to nextjs.org →
-				</a>
-			</footer>
+		<div className='overflow-hidden'>
+			<div className='h-dvh w-full bg-black flex justify-center items-center text-white text-4xl'>
+				Section 1
+			</div>
+
+			<div className='h-dvh w-full bg-black flex justify-center items-center text-white text-4xl'>
+				Section 2
+			</div>
+			<div ref={ref} className='h-dvh relative'>
+				<motion.div style={{ scaleX: scale }}>
+					<Image src='/1.webp' alt='1' fill className='w-full' />
+				</motion.div>
+			</div>
 		</div>
 	);
 }
